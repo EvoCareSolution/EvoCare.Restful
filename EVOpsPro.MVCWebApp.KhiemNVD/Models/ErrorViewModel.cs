f4ff7905606cf6258d642ff6e035211285251d43
DuyKhiem <khiemnvdse180168@fpt.edu.vn>
2025-11-06 09:51:00 +0700

    feat(web): setup MVC web application structure

---
+++ EVOpsPro.MVCWebApp.KhiemNVD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EVOpsPro.MVCWebApp.KhiemNVD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EVOpsPro.MVCWebApp.KhiemNVD/Models/LoginRequest.cs
@@ -0,0 +1,14 @@
+namespace EVOpsPro.MVCWebApp.KhiemNVD.Models
+{
+    
+        public class LoginRequest
+        {
+            /*
+             UserName = {userName, email, phone, employeeCode}
+             */
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
+    
+
+}
